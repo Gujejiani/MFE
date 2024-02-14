2910645f1e93b2997377636e3f958ca2422a1d64
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-02-14 23:53:57 +0400

    added connection using module federation

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin   = require('webpack/lib/container/ModuleFederationPlugin');
 module.exports = {
     mode: 'development',
     devServer: {
@@ -8,5 +9,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
         }),
+        new ModuleFederationPlugin({
+            name: 'products',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProductsIndex': './src/index',
+            },
+            shared: ['faker'],
+        })
     ],
 }
